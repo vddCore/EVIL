1a69b43ccbccfb3cf6d94ccf1393cd389eafa100
vddCore <vddcore@outlook.com>
2023-01-12 12:27:45 +0100

    fix a few minor bugs and update scripts to the new lang version

--- VirtualMachine/EVIL.UnitTests/Locals.cs
+++ VirtualMachine/EVIL.UnitTests/Locals.cs
@@ -1,4 +1,7 @@
+using EVIL.ExecutionEngine;
+using EVIL.ExecutionEngine.Abstraction;
 using EVIL.UnitTests.Base;
+using EVIL.UnitTests.Helper;
 using NUnit.Framework;
 
 namespace EVIL.UnitTests
@@ -8,7 +11,10 @@ namespace EVIL.UnitTests
         [Test]
         public void GlobalScopeLocalIsDefinedInRootChunk()
         {
+            var val = EVM.Evaluate("loc x = 10; ret x;");
             
+            XAssert.DoesNotExistIn(EVM.GlobalTable, "x");
+            XAssert.AreEqual(10, val);
         }
     }
 }
