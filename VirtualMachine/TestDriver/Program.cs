c309aa6f2182c76d143c6a1791a851709cae1d2c
vddCore <vddcore@outlook.com>
2023-01-12 12:27:44 +0100

    something something working vm closer

--- VirtualMachine/TestDriver/Program.cs
+++ VirtualMachine/TestDriver/Program.cs
@@ -10,11 +10,11 @@ namespace EVIL.VirtualMachine.TestDriver
         public static void Main(string[] args)
         {
             var lexer = new Lexer();
-            var parser = new Parser(lexer);
+            var parser = new Parser(lexer, true);
             var compiler = new Compiler();
             var disasm = new Disassembler();
             
-            lexer.LoadSource("fn main(a, b, c) { a = $@(-24 + 30 * 3 / 22) + \"test\"; b = 1200; c = 24 + 450 / 23.1 - 6.9 + 21.37 * 4.20; } main(1, 2, 3);");
+            lexer.LoadSource("fn main(a, b, c) { a = $@(-24 + 30 * 3 / 22) + 2; b = 1200; c = 24 + 450 / 23.1 - 6.9 + 21.37 * 4.20; } main(1, 2, 3);");
             
             var programTreeRoot = parser.Parse();
             var executable = compiler.Compile(programTreeRoot);
