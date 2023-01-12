e9f8f88b1f93ba7b56afd887281106315ba01573
vddCore <vddcore@outlook.com>
2023-01-12 12:27:43 +0100

    optimize indexing logic, fix clr function reassingment

--- EVIL/AST/Nodes/IndexingNode.cs
+++ EVIL/AST/Nodes/IndexingNode.cs
@@ -6,14 +6,14 @@ namespace EVIL.AST.Nodes
     public class IndexingNode : AstNode
     {
         public AstNode Indexable { get; }
-        public Queue<AstNode> KeyExpressions { get; }
+        public AstNode KeyExpression { get; }
 
         public bool WillBeAssigned { get; }
 
-        public IndexingNode(AstNode indexable, Queue<AstNode> keyExpressions, bool willBeAssigned)
+        public IndexingNode(AstNode indexable, AstNode keyExpression, bool willBeAssigned)
         {
             Indexable = indexable;
-            KeyExpressions = keyExpressions;
+            KeyExpression = keyExpression;
 
             WillBeAssigned = willBeAssigned;
         }
