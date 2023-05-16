5c73ece5f47571a5c39a3a755353e0cc7d226b76
vddCore <vddcore@outlook.com>
2023-05-16 06:42:32 +0200

    make stringpool iequatable

--- VirtualMachine/Ceres/ExecutionEngine/Diagnostics/StringPool.cs
+++ VirtualMachine/Ceres/ExecutionEngine/Diagnostics/StringPool.cs
@@ -1,12 +1,13 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Ceres.ExecutionEngine.Diagnostics
 {
-    public sealed class StringPool
+    public sealed class StringPool : IEquatable<StringPool>
     {
-        private Dictionary<int, string> _forward = new();
-        private Dictionary<string, int> _backward = new();
+        private readonly Dictionary<int, string> _forward = new();
+        private readonly Dictionary<string, int> _backward = new();
 
         public int Count => _forward.Count;
 
@@ -24,7 +25,7 @@ namespace Ceres.ExecutionEngine.Diagnostics
         public StringPool()
         {
         }
-        
+
         public StringPool(string[] values)
         {
             for (var i = 0; i < values.Length; i++)
@@ -37,7 +38,7 @@ namespace Ceres.ExecutionEngine.Diagnostics
                 return id;
 
             id = _forward.Count;
-            
+
             _forward.Add(id, value);
             _backward.Add(value, id);
 
@@ -46,5 +47,29 @@ namespace Ceres.ExecutionEngine.Diagnostics
 
         public string[] ToArray()
             => _forward.Values.ToArray();
+
+        public bool Equals(StringPool? other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+
+            return _forward.SequenceEqual(other._forward)
+                   && _backward.SequenceEqual(other._backward);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return ReferenceEquals(this, obj)
+                   || obj is StringPool other && Equals(other);
+        }
+
+        public override int GetHashCode()
+            => HashCode.Combine(_forward, _backward);
+
+        public static bool operator ==(StringPool? left, StringPool? right)
+            => Equals(left, right);
+
+        public static bool operator !=(StringPool? left, StringPool? right)
+            => !Equals(left, right);
     }
 }
