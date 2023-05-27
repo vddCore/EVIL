26a50ee99873da880bd374aa10e5dd0e20354fdf
vddCore <vddcore@outlook.com>
2023-05-27 16:58:04 +0200

    add more math runtime functions and corresponding tests...

--- VirtualMachine/Ceres.RuntimeTests/RuntimeModules/MathModuleTest.cs
+++ VirtualMachine/Ceres.RuntimeTests/RuntimeModules/MathModuleTest.cs
@@ -15,7 +15,43 @@ namespace Ceres.RuntimeTests.RuntimeModules
                 Is.EqualTo(2137)
             );
         }
+
+        [Test]
+        public void Acos()
+        {
+            Assert.That(
+                Math.Round(RunEvilCode("fn test() -> math.acos(0.13);").Number, 1),
+                Is.EqualTo(1.4)
+            );
+        }
+
+        [Test]  
+        public void Acosh()
+        {
+            Assert.That(
+                Math.Round(RunEvilCode("fn test() -> math.acosh(100);").Number, 1),
+                Is.EqualTo(5.3)
+            );
+        }
+
+        [Test]
+        public void Asin()
+        {
+            Assert.That(
+                Math.Round(RunEvilCode("fn test() -> math.asin(0.13);").Number, 1),
+                Is.EqualTo(0.1)
+            );
+        }
         
+        [Test]
+        public void Asinh()
+        {
+            Assert.That(
+                Math.Round(RunEvilCode("fn test() -> math.asinh(0.20);").Number, 1),
+                Is.EqualTo(0.2)
+            );
+        }
+
         [Test]
         public void Cos()
         {
@@ -24,7 +60,7 @@ namespace Ceres.RuntimeTests.RuntimeModules
                 Is.EqualTo(0.9)
             );
         }
-        
+
         [Test]
         public void Sin()
         {
@@ -38,11 +74,11 @@ namespace Ceres.RuntimeTests.RuntimeModules
         public void SinCos()
         {
             var ret = RunEvilCode("fn test() -> math.sincos(math.pi/6);").Table!;
-            
+
             Assert.That(Math.Round(ret["sin"].Number, 1), Is.EqualTo(0.5));
             Assert.That(Math.Round(ret["cos"].Number, 1), Is.EqualTo(0.9));
         }
-        
+
         [Test]
         public void Sqrt()
         {
