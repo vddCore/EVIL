8e8489e538575a9d199dd220ac08b1190304ea0b
vddCore <vddcore@outlook.com>
2023-05-28 18:16:45 +0200

    add string module beginnings and associated tests

---
+++ VirtualMachine/Ceres.Runtime/Modules/StringModule.cs
@@ -0,0 +1,34 @@
+﻿using Ceres.ExecutionEngine.Collections;
+using Ceres.ExecutionEngine.Concurrency;
+using Ceres.ExecutionEngine.TypeSystem;
+using Ceres.Runtime.Extensions;
+
+namespace Ceres.Runtime.Modules
+{
+    public sealed class StringModule : RuntimeModule
+    {
+        public override string FullyQualifiedName => "str";
+
+        public StringModule()
+        {
+            AddGetter("empty", (_) => string.Empty);
+        }
+
+        [RuntimeModuleFunction("spl")]
+        private static DynamicValue Split(Fiber _, params DynamicValue[] args)
+        {
+            args.ExpectStringAt(0, out var src)
+                .ExpectStringAt(1, out var delim);
+
+            var segments = src.Split(delim);
+            var table = new Table();
+
+            for (var i = 0; i < segments.Length; i++)
+            {
+                table.Add(i, segments[i]);
+            }
+
+            return table;
+        }
+    }
+}
---
+++ VirtualMachine/Ceres.RuntimeTests/RuntimeModules/StringModuleTest.cs
@@ -0,0 +1,20 @@
+﻿using Ceres.Runtime.Modules;
+using NUnit.Framework;
+
+namespace Ceres.RuntimeTests.RuntimeModules
+{
+    public class StringModuleTest : ModuleTest<StringModule>
+    {
+        [Test]
+        public void Split()
+        {
+            var t = RunEvilCode(
+                "fn test() -> str.spl(\"abc|def|ghi\", \"|\");"
+            ).Table!;
+
+            Assert.That(t[0].String, Is.EqualTo("abc"));
+            Assert.That(t[1].String, Is.EqualTo("def"));
+            Assert.That(t[2].String, Is.EqualTo("ghi"));
+        }
+    }
+}
