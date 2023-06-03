5dbbf866c219a1a3e3f515e3207110adbcd35d66
vddCore <vddcore@outlook.com>
2023-06-03 23:50:39 +0200

    reorganize function argument extensions, add more optionals, create missing type constraints.

---
+++ VirtualMachine/Ceres.Runtime/Extensions/FunctionArguments.Bounds.cs
@@ -0,0 +1,47 @@
+﻿using Ceres.ExecutionEngine.TypeSystem;
+using static Ceres.ExecutionEngine.TypeSystem.DynamicValue;
+
+namespace Ceres.Runtime.Extensions
+{
+    public static partial class FunctionArguments
+    {
+        public static DynamicValue[] ExpectExactly(this DynamicValue[] args, int count)
+        {
+            if (count != args.Length)
+                throw new EvilRuntimeException($"Expected {(count == 0 ? "no" : count)} arguments, found {args.Length}.");
+
+            return args;
+        }
+
+        public static DynamicValue[] ExpectAtLeast(this DynamicValue[] args, int count)
+        {
+            if (args.Length < count)
+                throw new EvilRuntimeException($"Expected at least {count} arguments, found {args.Length}.");
+
+            return args;
+        }
+
+        public static DynamicValue[] ExpectAtMost(this DynamicValue[] args, int count)
+        {
+            if (args.Length > count)
+                throw new EvilRuntimeException($"Expected at most {count} arguments, found {args.Length}.");
+
+            return args;
+        }
+
+        public static DynamicValue[] ExpectNone(this DynamicValue[] args)
+            => args.ExpectExactly(0);
+
+        public static DynamicValue[] ExpectTypeAt(this DynamicValue[] args, int index, DynamicValueType type)
+        {
+            args.ExpectAtLeast(index + 1);
+
+            var argType = args[index].Type;
+            
+            if (argType != type)
+                throw new EvilRuntimeException($"Expected a {type} at argument index {index}, found {argType}.");
+
+            return args;
+        }
+    }
+}
---
+++ VirtualMachine/Ceres.Runtime/Extensions/FunctionArguments.Convert.cs
@@ -0,0 +1,18 @@
+﻿using Ceres.ExecutionEngine.Collections;
+using Ceres.ExecutionEngine.TypeSystem;
+
+namespace Ceres.Runtime.Extensions
+{
+    public static partial class FunctionArguments
+    {
+        public static Table ToTable(this DynamicValue[] args)
+        {
+            var ret = new Table();
+
+            for (var i = 0; i < args.Length; i++)
+                ret[i] = args[i];
+            
+            return ret;
+        }
+    }
+}
