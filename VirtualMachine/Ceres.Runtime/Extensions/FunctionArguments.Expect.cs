cfa1d09ca9d3b01d61a03483595dbd810e89fe12
vddCore <vddcore@outlook.com>
2023-06-03 23:50:39 +0200

    reorganize function argument extensions, add more optionals, create missing type constraints.

--- VirtualMachine/Ceres.Runtime/Extensions/FunctionArguments.Expect.cs
+++ VirtualMachine/Ceres.Runtime/Extensions/FunctionArguments.Expect.cs
@@ -6,60 +6,11 @@ using static Ceres.ExecutionEngine.TypeSystem.DynamicValue;
 
 namespace Ceres.Runtime.Extensions
 {
-    public static class FunctionArgumentExtensions
+    public static partial class FunctionArguments
     {
-        public static Table ToTable(this DynamicValue[] args)
-        {
-            var ret = new Table();
-
-            for (var i = 0; i < args.Length; i++)
-                ret[i] = args[i];
-            
-            return ret;
-        }
-        
-        public static DynamicValue[] ExpectExactly(this DynamicValue[] args, int count)
-        {
-            if (count != args.Length)
-                throw new EvilRuntimeException($"Expected {(count == 0 ? "no" : count)} arguments, found {args.Length}.");
-
-            return args;
-        }
-
-        public static DynamicValue[] ExpectAtLeast(this DynamicValue[] args, int count)
-        {
-            if (args.Length < count)
-                throw new EvilRuntimeException($"Expected at least {count} arguments, found {args.Length}.");
-
-            return args;
-        }
-
-        public static DynamicValue[] ExpectAtMost(this DynamicValue[] args, int count)
-        {
-            if (args.Length > count)
-                throw new EvilRuntimeException($"Expected at most {count} arguments, found {args.Length}.");
-
-            return args;
-        }
-
-        public static DynamicValue[] ExpectNone(this DynamicValue[] args)
-            => args.ExpectExactly(0);
-
-        public static DynamicValue[] ExpectTypeAt(this DynamicValue[] args, int index, DynamicValueType type)
-        {
-            args.ExpectAtLeast(index + 1);
-
-            var argType = args[index].Type;
-            
-            if (argType != type)
-                throw new EvilRuntimeException($"Expected a {type} at argument index {index}, found {argType}.");
-
-            return args;
-        }
-
         public static DynamicValue[] ExpectNilAt(this DynamicValue[] args, int index)
             => args.ExpectTypeAt(index, DynamicValueType.Nil);
-
+        
         public static DynamicValue[] ExpectNumberAt(this DynamicValue[] args, int index, out double value)
         {
             args.ExpectTypeAt(index, DynamicValueType.Number);
@@ -74,23 +25,8 @@ namespace Ceres.Runtime.Extensions
             var num = args[index].Number;
             if (num % 1 != 0)
                 throw new EvilRuntimeException($"Expected an integer at argument index {index}.");
-            
-            value = (long)args[index].Number;
-            return args;
-        }
-        
-        public static DynamicValue[] OptionalIntegerAt(this DynamicValue[] args, int index, long defaultValue, out long value)
-        {
-            value = defaultValue;
-            
-            if (index < args.Length)
-            {
-                if (args[index] == Nil)
-                    return args;
-                
-                args.ExpectIntegerAt(index, out value);
-            }
 
+            value = (long)args[index].Number;
             return args;
         }
 
@@ -98,7 +34,7 @@ namespace Ceres.Runtime.Extensions
         {
             args.ExpectTypeAt(index, DynamicValueType.String);
             value = args[index].String!;
-            
+
             return args;
         }
 
@@ -109,7 +45,7 @@ namespace Ceres.Runtime.Extensions
             var str = args[index].String!;
             if (str.Length != 1)
                 throw new EvilRuntimeException($"Expected a single character at argument index {index}.");
-            
+
             value = str[0];
             return args;
         }
@@ -118,21 +54,6 @@ namespace Ceres.Runtime.Extensions
         {
             args.ExpectTypeAt(index, DynamicValueType.Boolean);
             value = args[index].Boolean;
-
-            return args;
-        }
-        
-        public static DynamicValue[] OptionalBooleanAt(this DynamicValue[] args, int index, bool defaultValue, out bool value)
-        {
-            value = defaultValue;
-            
-            if (index < args.Length)
-            {
-                if (args[index] == Nil)
-                    return args;
-                
-                args.ExpectBooleanAt(index, out value);
-            }
 
             return args;
         }
@@ -161,11 +82,20 @@ namespace Ceres.Runtime.Extensions
             return args;
         }
 
-        public static DynamicValue[] ExpectNativeFunctionAt(this DynamicValue[] args, int index, out NativeFunction value)
+        public static DynamicValue[] ExpectNativeFunctionAt(this DynamicValue[] args, int index,
+            out NativeFunction value)
         {
             args.ExpectTypeAt(index, DynamicValueType.NativeFunction);
             value = args[index].NativeFunction!;
-            
+
+            return args;
+        }
+
+        public static DynamicValue[] ExpectNativeObjectAt(this DynamicValue[] args, int index, out object? value)
+        {
+            args.ExpectTypeAt(index, DynamicValueType.NativeObject);
+            value = args[index].NativeObject;
+
             return args;
         }
     }
---
+++ VirtualMachine/Ceres.Runtime/Extensions/FunctionArguments.Optional.cs
@@ -0,0 +1,131 @@
+﻿using Ceres.ExecutionEngine.Collections;
+using Ceres.ExecutionEngine.Concurrency;
+using Ceres.ExecutionEngine.Diagnostics;
+using Ceres.ExecutionEngine.TypeSystem;
+using static Ceres.ExecutionEngine.TypeSystem.DynamicValue;
+
+namespace Ceres.Runtime.Extensions
+{
+    public static partial class FunctionArguments
+    {
+        public static DynamicValue[] OptionalIntegerAt(this DynamicValue[] args, int index, long defaultValue, out long value)
+        {
+            value = defaultValue;
+            
+            if (index < args.Length)
+            {
+                if (args[index] == Nil)
+                    return args;
+                
+                args.ExpectIntegerAt(index, out value);
+            }
+
+            return args;
+        }
+        
+        public static DynamicValue[] OptionalStringAt(this DynamicValue[] args, int index, string defaultValue, out string value)
+        {
+            value = defaultValue;
+            
+            if (index < args.Length)
+            {
+                if (args[index] == Nil)
+                    return args;
+                
+                args.ExpectStringAt(index, out value);
+            }
+
+            return args;
+        }
+        
+        public static DynamicValue[] OptionalCharAt(this DynamicValue[] args, int index, char defaultValue, out char value)
+        {
+            value = defaultValue;
+            
+            if (index < args.Length)
+            {
+                if (args[index] == Nil)
+                    return args;
+                
+                args.ExpectCharAt(index, out value);
+            }
+
+            return args;
+        }
+        
+        public static DynamicValue[] OptionalBooleanAt(this DynamicValue[] args, int index, bool defaultValue, out bool value)
+        {
+            value = defaultValue;
+            
+            if (index < args.Length)
+            {
+                if (args[index] == Nil)
+                    return args;
+                
+                args.ExpectBooleanAt(index, out value);
+            }
+
+            return args;
+        }
+        
+        public static DynamicValue[] OptionalTableAt(this DynamicValue[] args, int index, Table defaultValue, out Table value)
+        {
+            value = defaultValue;
+            
+            if (index < args.Length)
+            {
+                if (args[index] == Nil)
+                    return args;
+                
+                args.ExpectTableAt(index, out value);
+            }
+
+            return args;
+        }
+        
+        public static DynamicValue[] OptionalFiberAt(this DynamicValue[] args, int index, Fiber defaultValue, out Fiber value)
+        {
+            value = defaultValue;
+            
+            if (index < args.Length)
+            {
+                if (args[index] == Nil)
+                    return args;
+                
+                args.ExpectFiberAt(index, out value);
+            }
+
+            return args;
+        }
+        
+        public static DynamicValue[] OptionalChunkAt(this DynamicValue[] args, int index, Chunk defaultValue, out Chunk value)
+        {
+            value = defaultValue;
+            
+            if (index < args.Length)
+            {
+                if (args[index] == Nil)
+                    return args;
+                
+                args.ExpectChunkAt(index, out value);
+            }
+
+            return args;
+        }
+        
+        public static DynamicValue[] OptionalNativeFunctionAt(this DynamicValue[] args, int index, NativeFunction defaultValue, out NativeFunction value)
+        {
+            value = defaultValue;
+            
+            if (index < args.Length)
+            {
+                if (args[index] == Nil)
+                    return args;
+                
+                args.ExpectNativeFunctionAt(index, out value);
+            }
+
+            return args;
+        }
+    }
+}
