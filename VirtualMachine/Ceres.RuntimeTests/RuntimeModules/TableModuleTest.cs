3d642e7f597975d646e297fbc6ccd1a17b9f99b4
vddCore <vddcore@outlook.com>
2023-08-04 07:33:33 +0200

    add table clear runtime function

---
+++ VirtualMachine/Ceres.RuntimeTests/RuntimeModules/TableModuleTest.cs
@@ -0,0 +1,23 @@
+﻿using Ceres.Runtime.Modules;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Ceres.RuntimeTests.RuntimeModules
+{
+    public class TableModuleTest : ModuleTest<TableModule>
+    {
+        [Test]
+        public void Clear()
+        {
+            var t = EvilTestResult(
+                "fn test() {" +
+                "   var t = { 1, 2, 3, 4 };" +
+                "   tbl.clear(t);" +
+                "   ret t;" +
+                "}"
+            ).Table!;
+
+            t.Length.ShouldBe(0);
+        }
+    }
+}
