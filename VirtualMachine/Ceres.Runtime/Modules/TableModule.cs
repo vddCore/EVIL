bd5efe969ce43e866dfd493ec20fdb451e7b23f1
vddCore <vddcore@outlook.com>
2023-08-04 07:33:33 +0200

    add table clear runtime function

--- VirtualMachine/Ceres.Runtime/Modules/TableModule.cs
+++ VirtualMachine/Ceres.Runtime/Modules/TableModule.cs
@@ -1,8 +1,22 @@
-﻿namespace Ceres.Runtime.Modules
+﻿using Ceres.ExecutionEngine.Concurrency;
+using Ceres.ExecutionEngine.TypeSystem;
+using Ceres.Runtime.Extensions;
+
+namespace Ceres.Runtime.Modules
 {
     public class TableModule : RuntimeModule
     {
         public override string FullyQualifiedName => "tbl";
 
+        [RuntimeModuleFunction("clear", ReturnType = DynamicValue.DynamicValueType.Nil)]
+        private static DynamicValue Clear(Fiber _, params DynamicValue[] args)
+        {
+            args.ExpectExactly(1)
+                .ExpectTableAt(0, out var table);
+            
+            table.Clear();
+           
+            return DynamicValue.Nil;
+        }
     }
 }
