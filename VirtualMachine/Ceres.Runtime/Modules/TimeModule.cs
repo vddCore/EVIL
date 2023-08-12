b3c35589d6b424425d6010200aa0384064884a1c
vddCore <vddcore@outlook.com>
2023-08-12 21:53:28 +0200

    add a 'now' getter to time module, with a corresponding test.

--- VirtualMachine/Ceres.Runtime/Modules/TimeModule.cs
+++ VirtualMachine/Ceres.Runtime/Modules/TimeModule.cs
@@ -1,4 +1,5 @@
-﻿using Ceres.ExecutionEngine.Concurrency;
+﻿using Ceres.ExecutionEngine.Collections;
+using Ceres.ExecutionEngine.Concurrency;
 using Ceres.ExecutionEngine.TypeSystem;
 using Ceres.Runtime.Extensions;
 using static Ceres.ExecutionEngine.TypeSystem.DynamicValue;
@@ -9,12 +10,46 @@ namespace Ceres.Runtime.Modules
     {
         public override string FullyQualifiedName => "time";
 
+        public TimeModule()
+        {
+            AddGetter("now", (_) => CreateDateTimeTable(DateTime.Now));
+        }
+
         [RuntimeModuleFunction("stamp", ReturnType = DynamicValueType.Number)]
         private static DynamicValue Timestamp(Fiber fiber, params DynamicValue[] args)
         {
             args.ExpectNone();
-            
+
             return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
         }
+
+        private static Table CreateDateTimeTable(DateTime dt)
+        {
+            var ret = new Table
+            {
+                { "day", dt.Day },
+                { "month", dt.Month },
+                { "year", dt.Year },
+                { "hour", dt.Hour },
+                { "minute", dt.Minute },
+                { "second", dt.Second },
+                { "millisecond", dt.Millisecond },
+                { "microsecond", dt.Microsecond },
+                { "nanosecond", dt.Nanosecond },
+                { "ticks", dt.Ticks },
+                { "day_of_year", dt.DayOfYear },
+                { "day_of_week", dt.DayOfWeek.ToString() },
+                {
+                    "as_stamp", 
+                    new((context, args) =>
+                    {
+                        args.ExpectNone();
+                        return new DateTimeOffset(dt).ToUnixTimeMilliseconds();
+                    })
+                }
+            };
+
+            return ret;
+        }
     }
-}   
+}
---
+++ VirtualMachine/Ceres.RuntimeTests/RuntimeModules/TimeModuleTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Ceres.Runtime.Modules;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Ceres.RuntimeTests.RuntimeModules
+{
+    public class TimeModuleTest : ModuleTest<TimeModule>
+    {
+        [Test]
+        public void TimeNow()
+        {
+            var now = DateTime.Now;
+            var stamp = new DateTimeOffset(now).ToUnixTimeMilliseconds();
+
+            var t = EvilTestResult(
+                "fn test() {" +
+                "   var t = time.now;" +
+                "   ret { now: t, stamp: t.as_stamp() }; " +
+                "}"
+            ).Table!;
+
+            var evilNow = t["now"].Table!;
+            evilNow["day"].ShouldBe(now.Day);
+            evilNow["month"].ShouldBe(now.Month);
+            evilNow["year"].ShouldBe(now.Year);
+            evilNow["hour"].ShouldBe(now.Hour);
+            evilNow["minute"].ShouldBe(now.Minute);
+            ((double)evilNow["second"]).ShouldBeInRange(now.Second, now.Second + 2);
+            ((double)evilNow["millisecond"]).ShouldBeInRange(0, 999);
+            ((double)evilNow["microsecond"]).ShouldBeInRange(0, 999);
+            ((double)evilNow["nanosecond"]).ShouldBeInRange(0, 1000);
+            ((double)evilNow["ticks"]).ShouldBeInRange(now.Ticks, now.Ticks + 20000000);
+            evilNow["day_of_year"].ShouldBe(now.DayOfYear);
+            evilNow["day_of_week"].ShouldBe(now.DayOfWeek.ToString());
+
+            var evilStamp = t["stamp"].Number!;
+            evilStamp.ShouldBeInRange(stamp, stamp + 100);
+        }
+    }
+}
