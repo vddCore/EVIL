aee86359588eb148ecd1e4b0f12e046c3615599c
vddCore <vddcore@outlook.com>
2023-08-13 06:51:40 +0200

    getters can now be added via attributes

---
+++ VirtualMachine/Ceres.Runtime/RuntimeModuleGetterAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Ceres.ExecutionEngine.TypeSystem;
+
+namespace Ceres.Runtime
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class RuntimeModuleGetterAttribute : Attribute
+    {
+        public string SubNameSpace { get; }
+        public DynamicValue.DynamicValueType ReturnType { get; set; }
+
+        public RuntimeModuleGetterAttribute(string subNameSpace)
+        {
+            SubNameSpace = subNameSpace;
+        }
+    }
+}
