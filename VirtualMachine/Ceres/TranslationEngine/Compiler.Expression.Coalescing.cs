671f282eb7989f12434bd6e201dcc3837829923f
vddCore <vddcore@outlook.com>
2023-11-04 11:41:58 +0100

    rename local variable to be correct

--- VirtualMachine/Ceres/TranslationEngine/Compiler.Expression.Coalescing.cs
+++ VirtualMachine/Ceres/TranslationEngine/Compiler.Expression.Coalescing.cs
@@ -7,18 +7,18 @@ namespace Ceres.TranslationEngine
     {
         public override void Visit(CoalescingExpression coalescingExpression)
         {
-            var valueNotNullLabel = Chunk.CreateLabel();
+            var valueNotNilLabel = Chunk.CreateLabel();
             
             Visit(coalescingExpression.Left);
             Chunk.CodeGenerator.Emit(OpCode.DUP);
             Chunk.CodeGenerator.Emit(OpCode.LDNIL);
             Chunk.CodeGenerator.Emit(OpCode.CNE);
-            Chunk.CodeGenerator.Emit(OpCode.TJMP, valueNotNullLabel);
+            Chunk.CodeGenerator.Emit(OpCode.TJMP, valueNotNilLabel);
             Chunk.CodeGenerator.Emit(OpCode.POP);
             Visit(coalescingExpression.Right);
             
             Chunk.UpdateLabel(
-                valueNotNullLabel, 
+                valueNotNilLabel, 
                 Chunk.CodeGenerator.IP
             );
         }
