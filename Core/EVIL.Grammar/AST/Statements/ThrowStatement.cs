638b958edcf475e9979153c2fde3bc37feeb493c
vddCore <vddcore@outlook.com>
2024-04-01 06:53:37 +0200

    Add lexical support.

---
+++ Core/EVIL.Grammar/AST/Statements/ThrowStatement.cs
@@ -0,0 +1,14 @@
+using EVIL.Grammar.AST.Base;
+
+namespace EVIL.Grammar.AST.Statements
+{
+    public class ThrowStatement : Statement
+    {
+        public Expression ThrownExpression { get; }
+
+        public ThrowStatement(Expression thrownExpression)
+        {
+            ThrownExpression = thrownExpression;
+        }
+    }
+}
