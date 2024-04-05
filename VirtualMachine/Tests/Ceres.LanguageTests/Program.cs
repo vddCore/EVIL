73059635164ccb8a38f28cb997c8b583e965d29e
vddCore <vddcore@outlook.com>
2024-04-05 17:35:20 +0200

    Added early test exit when specific steps fail.

--- VirtualMachine/Tests/Ceres.LanguageTests/Program.cs
+++ VirtualMachine/Tests/Ceres.LanguageTests/Program.cs
@@ -1,8 +1,15 @@
-﻿using Ceres.ExecutionEngine;
+﻿using System.Linq;
+using Ceres.ExecutionEngine;
 using Ceres.LanguageTests;
 
 var vm = new CeresVM();
 vm.Start();
 
-var testRunner = new TestRunner("tests", vm);
-return await testRunner.RunTests();
+return await new TestRunner(
+    vm, 
+    new TestRunnerOptions(
+        FailOnCompilationErrors: args.Contains("--fail-on-compiler-errors"),
+        FailOnTestErrors: args.Contains("--fail-on-test-errors"),
+        TestDirectoryRoot: "tests"
+    )
+).RunTests();
