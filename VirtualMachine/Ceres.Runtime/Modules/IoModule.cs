10aca60eb49876399f91cf42ccf3ca307dded48d
vddCore <vddcore@outlook.com>
2024-04-06 21:17:20 +0200

    Fix a race condition in front-end.

--- VirtualMachine/Ceres.Runtime/Modules/IoModule.cs
+++ VirtualMachine/Ceres.Runtime/Modules/IoModule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Ceres.ExecutionEngine.Collections;
 using Ceres.ExecutionEngine.Concurrency;
 using Ceres.ExecutionEngine.TypeSystem;
 using Ceres.Runtime.Extensions;
@@ -10,7 +11,7 @@ namespace Ceres.Runtime.Modules
     public class IoModule : RuntimeModule
     {
         public override string FullyQualifiedName => "io";
-        
+
         [RuntimeModuleFunction("print")]
         [EvilDocFunction(
             "Writes string representations of the provided values, separated by the tabulator character, to the standard output stream.",
@@ -25,7 +26,7 @@ namespace Ceres.Runtime.Modules
         private static DynamicValue Print(Fiber _, params DynamicValue[] args)
         {
             args.ExpectAtLeast(1);
-            
+
             var str = string.Join(
                 "\t",
                 args.Select(x => x.ConvertToString().String)
@@ -34,11 +35,12 @@ namespace Ceres.Runtime.Modules
             Console.Write(str);
             return str.Length;
         }
-        
+
         [RuntimeModuleFunction("println")]
         [EvilDocFunction(
             "Writes string representations of the provided values, separated by the tabulator character, followed by the platform's new line sequence, to the standard output stream.",
-            Returns = "Length of the printed string, including the tabulator characters, if any, and the new line sequence.",
+            Returns =
+                "Length of the printed string, including the tabulator characters, if any, and the new line sequence.",
             ReturnType = DynamicValueType.Number
         )]
         [EvilDocArgument(
@@ -55,8 +57,45 @@ namespace Ceres.Runtime.Modules
 
             Console.Write(str);
             Console.Write(Environment.NewLine);
-            
+
             return str.Length + Environment.NewLine.Length;
         }
+
+        [RuntimeModuleFunction("readkey")]
+        [EvilDocFunction(
+            "Waits until the user presses a key.",
+            Returns = "A table representing information about the pressed key.",
+            ReturnType = DynamicValueType.Table
+        )]
+        [EvilDocArgument(
+            "echo_to_stdout",
+            "Whether to echo the pressed key to the standard output stream.",
+            DynamicValueType.Boolean,
+            CanBeNil = false,
+            DefaultValue = "true"
+        )]
+        private static DynamicValue ReadKey(Fiber _, params DynamicValue[] args)
+        {
+            args.ExpectAtMost(1)
+                .OptionalBooleanAt(0, true, out var echoToStdout);
+
+            var key = Console.ReadKey(!echoToStdout);
+
+            return new Table
+            {
+                { "keycode", (int)key.Key },
+                { "char", key.KeyChar.ToString() },
+                { "modifiers", (int)key.Modifiers }
+            };
+        }
+
+        [RuntimeModuleFunction("readln")]
+        [EvilDocFunction(
+            "Waits until the user inputs a string and accepts it with Return key",
+            Returns = "A string containing the input entered by the user, or `nil` if no more lines are available..",
+            ReturnType = DynamicValueType.String
+        )]
+        private static DynamicValue ReadLine(Fiber _, params DynamicValue[] __)
+            => Console.ReadLine() ?? DynamicValue.Nil;
     }
 }
