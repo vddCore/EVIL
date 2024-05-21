aadba9699dd1fe6608582ec41fdb5fc6df0a5d63
vddCore <vddcore@outlook.com>
2024-05-21 05:51:37 +0200

    Move self-expression to a separate shared function.

---
+++ Core/EVIL.Grammar/Parsing/Expressions/Parser.SelfExpression.cs
@@ -0,0 +1,14 @@
+﻿using EVIL.Grammar.AST.Expressions;
+using EVIL.Lexical;
+
+namespace EVIL.Grammar.Parsing
+{
+    public partial class Parser
+    {
+        private SelfExpression SelfExpression()
+        {
+            var (line, col) = Match(Token.Self);
+            return new SelfExpression { Line = line, Column = col };
+        }
+    }
+}
