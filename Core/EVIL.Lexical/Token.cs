5d3615d552083621c65aaba1fab5af32db8518c0
vddCore <vddcore@outlook.com>
2024-07-03 12:24:43 +0200

    Add 'retry' control flow statement.

--- Core/EVIL.Lexical/Token.cs
+++ Core/EVIL.Lexical/Token.cs
@@ -118,7 +118,8 @@ namespace EVIL.Lexical
         public static readonly Token Try = new(TokenType.Try, TokenClass.Keyword, "try");
         public static readonly Token Catch = new(TokenType.Catch, TokenClass.Keyword, "catch");
         public static readonly Token Throw = new(TokenType.Throw, TokenClass.Keyword, "throw");
-
+        public static readonly Token Retry = new(TokenType.Retry, TokenClass.Keyword, "retry");
+        
         public static readonly Token Val = new(TokenType.Val, TokenClass.Keyword, "val");
         public static readonly Token If = new(TokenType.If, TokenClass.Keyword, "if");
         public static readonly Token Elif = new(TokenType.Elif, TokenClass.Keyword, "elif");
