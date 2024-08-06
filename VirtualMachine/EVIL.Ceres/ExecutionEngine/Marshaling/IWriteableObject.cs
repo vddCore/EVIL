b6345ac4bf9d0866cbebebe5d3e7ceb4f689047a
vddCore <vddcore@outlook.com>
2024-08-06 06:49:42 +0200

    Add interfaces for writeable and indexable objects.

---
+++ VirtualMachine/EVIL.Ceres/ExecutionEngine/Marshaling/IWriteableObject.cs
@@ -0,0 +1,9 @@
+﻿namespace EVIL.Ceres.ExecutionEngine.Marshaling;
+
+using EVIL.Ceres.ExecutionEngine.TypeSystem;
+
+public interface IWriteableObject
+{
+    DynamicValue this[DynamicValue key] { set; }
+    void Set(DynamicValue key, DynamicValue value);
+}
